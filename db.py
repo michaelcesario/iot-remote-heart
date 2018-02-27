b3301894ee561bb5bf11e5c5e035d8bd519d4d6e
michaelcesario <michael_c_4@live.ca>
2018-02-26 23:36:11 -0800

    change os env variables

--- db.py
+++ db.py
@@ -6,18 +6,12 @@ from urllib.parse import urlparse
 class DatabaseConnection:
     @classmethod
     def getDBCursor(cls):
-        url = urlparse(urlparse(os.environ['DATABASE_URL']))
-        dbname = url.path[1:]
-        user = url.username
-        password = url.password
-        host = url.hostname
-        port = url.port
 
         dbConnection = psycopg2.connect(
-            dbname=dbname,
-            user=user,
-            password=password,
-            host=host,
-            port=port
+            dbname=os.environ['DATABASE'],
+            user=os.environ['USER'],
+            password=os.environ['PASSWORD'],
+            host=os.environ['HOST'],
+            port=os.environ['PORT']
         )
         return dbConnection
