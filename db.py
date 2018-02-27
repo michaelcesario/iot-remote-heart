ec551a14a3fd3177689db75bd54083c2a9761e88
michaelcesario <michael_c_4@live.ca>
2018-02-26 23:49:52 -0800

    fixed url parsing

--- db.py
+++ db.py
@@ -6,12 +6,18 @@ from urllib.parse import urlparse
 class DatabaseConnection:
     @classmethod
     def getDBCursor(cls):
+        url = urlparse(os.environ['DATABASE_URL'])
+        dbname = url.path[1:]
+        user = url.username
+        password = url.password
+        host = url.hostname
+        port = url.port
 
         dbConnection = psycopg2.connect(
-            dbname=os.environ['DATABASE'],
-            user=os.environ['USER'],
-            password=os.environ['PASSWORD'],
-            host=os.environ['HOST'],
-            port=os.environ['PORT']
+            dbname=dbname,
+            user=user,
+            password=password,
+            host=host,
+            port=port
         )
         return dbConnection
